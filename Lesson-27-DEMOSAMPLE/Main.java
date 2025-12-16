aa3bfc002339952730338f0dbf793f33990629ff
DamaliBlake <damalib7@nycstudents.net>
2025-12-16 21:50:45 +0000

    work

--- Lesson-27-DEMOSAMPLE/Main.java
+++ Lesson-27-DEMOSAMPLE/Main.java
@@ -6,43 +6,47 @@ class Main {
 
   void init() {
 
-    String message = "attack! Take no prisoners.";
+    // Read original text
+    String file = Input.readFile("Original.txt");
 
-    // ENCODING
-    String step1 = reverse(message);
-    String step2 = encode(step1);
-    String step3 = vowelToNumber(step2);
-    String step4 = addAy(step3);
-    String step5 = wingdingsEncode(step4);
+    // ENCRYPTION
+    String e1 = swapLetters(file);
+    String e2 = encode(e1);
+    String e3 = vowelEncrypt(e2);
+    String e4 = addAy(e3);
+    String e5 = wingdingsEncrypt(e4);
+    String e6 = reverse(e5);
 
-    System.out.println("Encoded Message:");
-    System.out.println(step5);
+    Input.writeFile("Encoded.txt", e6);
 
-    // DECODING (reverse order)
-    String d1 = wingdingsDecode(step5);
-    String d2 = removeAy(d1);
-    String d3 = numberToVowel(d2);
-    String d4 = decode(d3);
-    String d5 = reverse(d4);
+    // DECRYPTION
+    String file2 = Input.readFile("Encoded.txt");
 
-    System.out.println("\nDecoded Message:");
-    System.out.println(d5);
+    String d1 = reverse(file2);
+    String d2 = wingdingsDecrypt(d1);
+    String d3 = removeAy(d2);
+    String d4 = vowelDecrypt(d3);
+    String d5 = decode(d4);
+    String d6 = swapLetters(d5);
+
+    Input.writeFile("Decoded.txt", d6);
   }
 
-  // 1. Reverse
-  String reverse(String txt) {
-    String bld = "";
-    for (int i = txt.length() - 1; i >= 0; i--) {
-      bld += txt.charAt(i);
+  // 1. Swap adjacent letters
+  String swapLetters(String txt) {
+    char[] arr = txt.toCharArray();
+    for (int i = 0; i < arr.length - 1; i += 2) {
+      char temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
     }
-    return bld;
+    return new String(arr);
   }
 
-  // 2. Caesar cipher (+2)
+  // 2. Caesar shift +2
   String encode(String txt) {
     String bld = "";
-    for (int i = 0; i < txt.length(); i++) {
-      char c = txt.charAt(i);
+    for (char c : txt.toCharArray()) {
       if (Character.isLetter(c)) {
         bld += (char)(c + 2);
       } else {
@@ -54,8 +58,7 @@ class Main {
 
   String decode(String txt) {
     String bld = "";
-    for (int i = 0; i < txt.length(); i++) {
-      char c = txt.charAt(i);
+    for (char c : txt.toCharArray()) {
       if (Character.isLetter(c)) {
         bld += (char)(c - 2);
       } else {
@@ -65,24 +68,20 @@ class Main {
     return bld;
   }
 
-  // 3. Vowels → Numbers
-  String vowelToNumber(String txt) {
-    return txt.replace('a','1')
-              .replace('e','2')
-              .replace('i','3')
-              .replace('o','4')
-              .replace('u','5');
+  // 3. Replace vowels with numbers
+  String vowelEncrypt(String txt) {
+    return txt.replace("a","1").replace("e","2")
+              .replace("i","3").replace("o","4")
+              .replace("u","5");
   }
 
-  String numberToVowel(String txt) {
-    return txt.replace('1','a')
-              .replace('2','e')
-              .replace('3','i')
-              .replace('4','o')
-              .replace('5','u');
+  String vowelDecrypt(String txt) {
+    return txt.replace("1","a").replace("2","e")
+              .replace("3","i").replace("4","o")
+              .replace("5","u");
   }
 
-  // 4. Add "ay"
+  // 4. Add "ay" to each word
   String addAy(String txt) {
     String[] words = txt.split(" ");
     String bld = "";
@@ -96,26 +95,31 @@ class Main {
     String[] words = txt.split(" ");
     String bld = "";
     for (String w : words) {
-      bld += w.substring(0, w.length() - 2) + " ";
+      if (w.endsWith("ay")) {
+        bld += w.substring(0, w.length() - 2) + " ";
+      }
     }
     return bld.trim();
   }
 
-  // 5. Wingdings (simulated symbol encoding)
-  String wingdingsEncode(String txt) {
-    String bld = "";
-    for (int i = 0; i < txt.length(); i++) {
-      char c = txt.charAt(i);
-      bld += (char)(c + 100);
-    }
-    return bld;
+  // 5. Wingdings (simulated symbol mapping)
+  String wingdingsEncrypt(String txt) {
+    return txt.replace("a","@").replace("b","#")
+              .replace("c","$").replace("d","%")
+              .replace("e","&");
   }
 
-  String wingdingsDecode(String txt) {
+  String wingdingsDecrypt(String txt) {
+    return txt.replace("@","a").replace("#","b")
+              .replace("$","c").replace("%","d")
+              .replace("&","e");
+  }
+
+  // 6. Reverse string
+  String reverse(String txt) {
     String bld = "";
-    for (int i = 0; i < txt.length(); i++) {
-      char c = txt.charAt(i);
-      bld += (char)(c - 100);
+    for (int i = txt.length() - 1; i >= 0; i--) {
+      bld += txt.charAt(i);
     }
     return bld;
   }
